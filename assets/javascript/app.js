ec998019f97498ec8a8c5a0df37a0220648490eb
Jenny Na <jennyna13@gmail.com>
2018-12-10 06:49:05 -0500

    commiting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,71 +1,74 @@
 $(document).ready(function(){
 
-    var correct = 0;
-    var wrong = 0;
-    var unanswered = 0;
-    var userGuess = "";
-    var index;
-    var pick;
     var Timer = 10;
     var intervalId;
+
+    var game = {
+        currentQuestion: 0,
+        counter: countStartNum,
+        correct: 0,
+        incorrect: 0,
+        unanswered: 0
+    }
+
     var trivia = [{
         photo:"", 
         question: "Which of the following has not been the name of the hospital?",
         chocies: ["Seattle Grace Mercy West Hospital", "Seattle Grace Hospital", "Grey Sloan Memorial Hospital", "Grey Shepherd Memorial Hospital"],
-        answer: 3,}, 
+        answer: "Grey Shepherd Memorial Hospital"}, 
         {
         photo: "",
         question: "Which of the following has Meredith not lived through?",
         choices:["A bomb", "A car accident","Drowning",],
-        answer: 1,
+        answer: "A car accident",
         },
         {
         photo: "",
         question: "Which of the following universities did Cristina Yang NOT earn a degree from?",
         choices: ["Smith College", "Yale University", "Standford University", "University of California at Berkely"],
-        answer:1,
+        answer:"Yale University",
         },
         {
         photo: "",
         question: "Where does Derek get an opportunity to work in Season 10?",
         choices: ["Washington, D.C.", "New York", "Arizona", "California"],
-        answer: 0,
+        answer: "Washington, D.C.",
         }, 
         {
         photo: "",
         question: "What nickname do the interns give Meredith?",
         choices: ["The Nazi", "Queen of Darkness", "Queen of General", "Medusa"],
-        answer: 3,
+        answer: "Medusa",
         },  
         {
         photo: "",
         question: "Which two of the original interns failed their boards?",
         choices: ["George and Izzie", "Alex, and Meredith", "George and Alex", "Alex and Izzie"],
-        answer: 2,
+        answer: "George and Alex",
         },
         {
         photo: "",
         question: "Izzie Stevens was diagnosed with stage IV metastatic melanoma in Season 5. Which symptom made her realize something was wrong?",
         choices: ["Hallucinations of her dead fiance", "Abdominal cramps and vomiting", "Headaches and nausea", "Rapid weight loss"],
-        answer: 0,
+        answer: "Hallucinations of her dead fiance",
         },
         {
         photo: "",
         question: "How many half-sisters does Meredith Grey have?",
         choices: ["One", "Two", "Three", "Four"],
-        answer: 2,
+        answer: "Three",
         },
         {
         photo: "",
         question: "How did Arizona Robbins lose her leg?",
         choices: ["Car crash", "Hurricane", "Earthquake", "Plane crash"],
-        answer: 3,
+        answer: "Plane crash",
         },
         {
         photo: "",
         question: "What did Derek always say before starting an operation?",
         choices: ["It's a beautiful day to save lives", "Let's save a life", "It's a good day", "It's a great day to save lives"],
-        answer: 0,
+        answer: "It's a beautiful day to save lives",
         }
      ];
     
@@ -74,59 +77,23 @@ $("#wrapper2").hide();
 $("#wrapper3").hide();
 
 $(".button2").on("click", function(){
-    nextQuestion();
     $("body").attr("background", "#");
     $("#wrapper").hide();
     $("#wrapper3").hide();
     $("#wrapper2").show();
     $(".timer").text(Timer);
-    $(".question").text("<h3>" + trivia[i].question + "</h3>");
-   $("#answer1").text("<h3>" + trivia[i].chocies[0]+ "</h3>");
-   $("#answer2").text("<h3>" + trivia[i].chocies[1]+ "</h3>");
-   $("#answer3").text("<h3>" + trivia[i].chocies[2]+ "</h3>");
-   $("#answer4").text("<h3>" + trivia[i].chocies[3]+ "</h3>");
-    $(".image").attr();
-    runTimer ();
-    countDown();
 }); 
 
- 
-    function nextQuestion(){
-    $(".question").show();
-    $(".timer").show();
-
-    $(".answer").empty().hide();
-    game.time_start();
-}
-
-    //when question set is over
-    if(trivia.question>9){
-        game.finish_restart();
-    }
-
-    //show question
-    $(".question_sec").append("<h3>"+questions[game.q_arr].question+"</h3>");
-
-
-    //show examples
-    for(var i=0; i<questions[game.q_arr].examples.length; i++){
-
-        var test = $("<p>").addClass("eg");
-        test.append("<button type='button' class='btn btn-outline-light'>"+questions[game.q_arr].examples[i]+"</button>");
-
-    $(".examples_sec").append(test);
-    };
-
-$(".answerButton").on("click", function(){
-
-});
-
-function runTimer (){
+function run() {
+    clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
+  }
+function stop() {
+	clearInterval(intervalId);
 }
-function countDown(){
-    $(".timer").text(Timer);
+function decrement(){
     Timer--;
+    $(".timer").text(Timer);
     if (Timer === 0) {
         stop();
         $("#wrapper").hide();
@@ -135,12 +102,6 @@ function countDown(){
         unanswered ++;
         $(".showAnswer").text("<h3>Time's Up!" + "</h3>")
     }
-}
-function answerChosen(){
-
-}
-function stop() {
-	clearInterval(intervalId);
 }
 
 });
